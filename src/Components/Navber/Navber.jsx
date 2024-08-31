a5651f30740427dee5f83bbd8815ce5e096a7c47
mohamed atef <atef21143@gmail.com>
2024-08-31 15:43:31 +0300

    modify the aria-hidden

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -38,7 +38,7 @@ export default function Navber() {
             aria-expanded={isNavOpen}
           >
             <span className="sr-only">Open main menu</span>
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <svg className="w-5 h-5" aria-hidden="false" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M1 1h15M1 7h15M1 13h15" />
             </svg>
           </button>
